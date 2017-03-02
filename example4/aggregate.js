03f8e2d55cbc3ade48fe07baac04231d72a75b1e
kirinrastogi <rastogikirin@gmail.com>
2017-03-02 22:23:56 +0000

    add example 4 base, create dir for example 3

---
+++ example4/aggregate.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Sortable = require('../models/Sortable')
+const MONGODB_URI = require('../config').MONGODB_URI
+
+mongoose.connect(MONGODB_URI)
+console.log(`Connected to ${MONGODB_URI}`)
+mongoose.Promise = Promise
+
+Sortable.aggregate([
+  {
+    $project: {
+      score: '$score',
+      value: '$value',
+      popularity: {
+        $divide: [ '$score', '$value' ]
+      }
+    }
+  },
+  { 
+    $sort: { popularity: -1} 
+  },
+])
+  .limit(10)
+  .then(console.log)
+  .catch(console.log)
