b9b1aec55e4692d8a523280f228fa5c547e07a74
kirinrastogi <rastogikirin@gmail.com>
2017-03-03 16:04:39 -0500

    Add example4 aggregation functions

--- example4/aggregate.js
+++ example4/aggregate.js
@@ -6,20 +6,33 @@ mongoose.Promise = Promise
 mongoose.connect(MONGODB_URI)
 console.log(`Connected to ${MONGODB_URI}`)
 
-Sortable.aggregate([
-  {
-    $project: {
-      score: '$score',
-      value: '$value',
-      popularity: {
-        $divide: [ '$score', '$value' ]
+function comparison(first, second) {
+  return ((first.score / first.value) < (second.score / second.value)) ? 1 : -1
+}
+
+function aggregateBadly() {
+  Sortable.find({})
+    .then(largeArray => largeArray.sort(comparison).splice(0, 10))
+    .then(console.log)
+    .catch(console.log)
+}
+
+function aggregateWell() {
+  Sortable.aggregate([
+    {
+      $project: {
+        score: '$score',
+        value: '$value',
+        popularity: {
+          $divide: [ '$score', '$value' ]
+        }
       }
-    }
-  },
-  { 
-    $sort: { popularity: -1} 
-  },
-])
-  .limit(10)
-  .then(console.log)
-  .catch(console.log)
+    },
+    { 
+      $sort: { popularity: -1} 
+    },
+  ])
+    .limit(10)
+    .then(console.log)
+    .catch(console.log)
+}
