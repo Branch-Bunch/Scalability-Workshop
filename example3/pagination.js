9232f559f07dcc3f0197fd67388f6c36afe9471f
kirinrastogi <rastogikirin@gmail.com>
2017-03-04 14:44:17 -0500

    add range, skip, implementations

--- example3/pagination.js
+++ example3/pagination.js
@@ -6,4 +6,17 @@ mongoose.Promise = Promise
 mongoose.connect(MONGODB_URI)
 console.log(`Connected to ${MONGODB_URI}`)
 
-Sortable.find({ createdAt: { } })
+function rangeAndLimit() {
+  Sortable.find({ createdAt: { $lte: (new Date('2017/03/15').getTime()) } })
+    .limit(10)
+    .then(console.log)
+    .catch(console.log)
+}
+
+function skipAndLimit() {
+  Sortable.find({})
+    .skip(10)
+    .limit(10)
+    .then(console.log)
+    .catch(console.log)
+}
