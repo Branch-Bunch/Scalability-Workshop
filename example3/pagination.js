4f6f3d3ef195882b80dc630ae0ddd2b0ae2c407f
Jacky Chiu <jacky.chiu888@gmail.com>
2017-03-04 21:59:25 -0500

    Bit of refactor

--- example3/pagination.js
+++ example3/pagination.js
@@ -6,19 +6,29 @@ mongoose.Promise = Promise
 mongoose.connect(MONGODB_URI)
 console.log(`Connected to ${MONGODB_URI}`)
 
-function rangeAndLimit() {
-  Sortable.find({ createdAt:
-    { $lte: (new Date('2017/03/15').getTime()) } 
-  })
+function rangeAndLimit(findOptions) {
+  Sortable.find(findOptions)
+    .sort({ createdAt: -1 })
     .limit(10)
     .then(console.log)
     .catch(console.log)
 }
 
-function skipAndLimit() {
+function skipAndLimit(page) {
+  const skipAmount = 10
   Sortable.find({})
-    .skip(10)
+    .sort({ createdAt: -1 })
+    .skip(skipAmount * page)
     .limit(10)
     .then(console.log)
     .catch(console.log)
+}
+
+// Client would tell you what page they want
+const page = 5 
+// Client would tell you this is last element 
+const findOptions = {
+  createdAt: { 
+    $lt: (new Date('2017/03/15').getTime()) 
+  } 
 }
